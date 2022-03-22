1b8481e9214d624497f23e8cd327105cb05cd937
Awesome Hacker <awesome.hacker@test.com>
2022-03-21 17:07:50 -0700

    No Azure AD

--- Program.cs
+++ Program.cs
@@ -16,7 +16,8 @@ string[] possibleAnswers = { "no", "i don't know", "don't know", "i do not know"
 
 if(!StorageService.HasEnrolledUser())
 {
-   userDetails = await SignInAndGetUserId();
+    Console.WriteLine("You need to authenticate first");
+    userDetails = await AuthenticationService.SignInAndGetUserId();
 }
 else
 {
@@ -39,7 +40,7 @@ if (ResponseIsNegative(result.Text))
     var username = await recognizer.RecognizeOnceAsync();
     if (!string.IsNullOrEmpty(username.Text))
     {
-        await VerificationEnroll(config, synthesizer, username.Text);
+        await VerificationEnroll(config, synthesizer);
     }
 }
 
@@ -115,38 +116,4 @@ async Task SpeakerVerify(SpeechConfig config, SpeechSynthesizer synthesizer)
     }
     //{profileMapping[result.ProfileId]}
     await synthesizer.SpeakTextAsync($"Congratulations, you have been authenticated with score {result.Score}");
-}
-
-async Task <(string Id, string Username)> SignInAndGetUserId()
-{
-    var clientId = "a79ccd89-0bb4-4909-92fa-737e246e3bec";
-    var tenantId = "b55f0c51-61a7-45c3-84df-33569b247796";
-    
-    IPublicClientApplication app = PublicClientApplicationBuilder
-        .Create(clientId)
-        .WithTenantId(tenantId)
-        .Build();
-    
-    AuthenticationResult result = null;
-    try
-    {
-        result = await app.AcquireTokenWithDeviceCode(new string[] {"user.read"}, 
-            deviceCodeCallback =>
-            {
-                Console.WriteLine(deviceCodeCallback.Message);
-                return Task.FromResult(0);
-            }).ExecuteAsync().ConfigureAwait(false);
-    }
-    catch(MsalServiceException ex)
-    {
-        Console.WriteLine($"MSAL error: {ex.Message}");
-    }
-
-    var handler = new JwtSecurityTokenHandler();
-    var idToken = handler.ReadJwtToken(result.IdToken);
-    return 
-    (
-        idToken.Claims.First(x => x.Type.Equals("oid")).Value, 
-        idToken.Claims.First(x => x.Type.Equals("preferred_username")).Value
-    );
 }
